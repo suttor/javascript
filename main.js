635de14531156afde1cfbf1ecd56a10fb8e5e3ce
Suttor <daniel.suttor@student.uibk.ac.at>
2018-04-11 17:25:58 +0200

    javascript 101

---
+++ main.js
@@ -0,0 +1,31 @@
+// alert("Hello World");
+
+let nachricht = "Hallo Welt";
+// alert(nachricht); 
+nachricht = "neuer Text"
+// alert(nachricht); 
+console.log(nachricht);
+const linkColor = "#ff0000";
+let eine_variable = "test";
+console.log("Variable: eine_variable", eine_variable);
+let highScore = 99999;
+console.log("Highscore ist: ", highScore / 3)
+let meineNachricht = "test1";
+console.log (meineNachricht);
+meineNachricht = "test2"
+console.log(meineNachricht);
+meineNachricht = `Ein mehr
+und ein
+ganzer langer
+text`
+
+console.log (meineNachricht)
+
+let fullName = 'Jeffrey "The Dude" Lebowsky'
+console.log (fullName);
+
+let isOver18 = true;
+console.log ("über 18?", isOver18);
+let age = 13
+console.log ("age über 18", age > 18);
+
